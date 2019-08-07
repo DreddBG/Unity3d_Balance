d36eadbbaff1bf509e4f9b521e2b1f282b63c354
DreddBG <ninjakiviWTF@gmail.com>
2019-08-07 22:15:15 +0300

    initial commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnee;
+    public int count = 0;
+
+    void Setup() {
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if(Input.GetMouseButton(0)) {
+            Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+        } 
+    }
+}
